9ed79aab07885e6b28e59351aaa49d98575a49a2
Prabhat Kumar Yadav <prabhat1298@gmail.com>
2025-06-14 12:36:54 +0530

    deploy server

--- src/Util/axiosClient.js
+++ src/Util/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://travel-server-nu.vercel.app/'
+  baseURL: 'https://travel-server-07iw.onrender.com'
 });
 
 export const getDestinations = async () => {
