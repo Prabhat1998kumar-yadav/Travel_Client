f5824615279178071d1a3be94a8a22d6fc7cc857
Prabhat Kumar Yadav <prabhat1298@gmail.com>
2025-06-14 12:17:54 +0530

    chage url

--- src/Util/axiosClient.js
+++ src/Util/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://travel-server-lime.vercel.app/'
+  baseURL: 'https://travel-server-nu.vercel.app/'
 });
 
 export const getDestinations = async () => {
